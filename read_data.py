185306ede00a9e61c20a758de419bf0031a7b00d
Lorenzo <naroloal94@gmail.com>
2019-08-01 18:34:45 -0300

    archivos para leer, crear data y vocabulario (en progreso)

---
+++ read_data.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import argparse
+import json
+import csv
+
+def main():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('sentences')
+    ap.add_argument('labels', nargs='?')
+
+    args = ap.parse_args()
+
+    sentence_data = open(args.sentences, 'r')
+    with open('train.txt','w') as f:
+        with open('vocabulary.txt','w') as v:
+            if args.labels:
+                label_data = open(args.labels, 'r')
+                for sentence, label in zip(it_sentences(sentence_data), it_labels(label_data)):
+            # Tenemos la oración en sentence con su categoría en label
+                    f.write(sentence + '|' + label +'\n')
+                    v.write(sentence + '\n')
+                    pass
+            else:
+                for sentence in it_sentences(sentence_data):
+                # Tenemos una oración en sentence
+                #print(sentence)
+                    pass
+    
+
+def it_sentences(sentence_data):
+    for line in sentence_data:
+        example = json.loads(line)
+        yield example['sentence2']
+
+def it_labels(label_data):
+    label_data_reader = csv.DictReader(label_data)
+    for example in label_data_reader:
+        yield example['gold_label']
+
+
+
+
+main()
