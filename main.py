df6686ed589826983bafc2d6a70effdfc4db5502
Lorenzo <naroloal94@gmail.com>
2019-08-01 17:54:41 -0300

    Lorenzo

--- main.py
+++ main.py
@@ -6,4 +6,11 @@ Este es un archivo temporal.
 """
 import json
 
+d = {}
+i = 0
+with open('snli_1.0_train_filtered.jsonl') as f:
+    json_data = [json.loads(line) for line in f]
+        
+    
 
+    
