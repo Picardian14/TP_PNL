0b1fa8085cb7180e2a52a8a6c54c423ad803078e
Lorenzo <naroloal94@gmail.com>
2019-08-09 22:38:07 -0300

    Arreglo del prepocesamiento de datos

--- data/read_data.py
+++ data/read_data.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
+##!/usr/bin/env python
 import argparse
 import json
 import csv
-
+path_to_data = '/home/mr-mister/Ivan/PLN_ECI2019/TP/TP_PNL'
 def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('sentences')
@@ -11,19 +11,20 @@ def main():
     args = ap.parse_args()
 
     sentence_data = open(args.sentences, 'r')
-    if args.labels:
-        label_data = open(args.labels, 'r')
-        for sentence, label in zip(it_sentences(sentence_data), it_labels(label_data)):
-            # Tenemos la oración en sentence con su categoría en label
-            #print(label, sentence)
-            pass
-    else:
-        for sentence in it_sentences(sentence_data):
-            # Tenemos una oración en sentence
-            #print(sentence)
-            pass
+    with open('val.txt','w') as f:
+        if args.labels:
+                label_data = open(args.labels, 'r')
+                for sentence, label in zip(it_sentences(sentence_data), it_labels(label_data)):
+                # Tenemos la oración en sentence con su categoría en label
+                        f.write(sentence  + '|' +label +'\n')
+                        pass
+        else:
+                for sentence in it_sentences(sentence_data):
+                # Tenemos una oración en sentence
+                        f.write(sentence + '\n')
+                #print(sentence)
+                        pass
     
-
 def it_sentences(sentence_data):
     for line in sentence_data:
         example = json.loads(line)
@@ -33,8 +34,6 @@ def it_labels(label_data):
     label_data_reader = csv.DictReader(label_data)
     for example in label_data_reader:
         yield example['gold_label']
-
-
 
 
 main()
